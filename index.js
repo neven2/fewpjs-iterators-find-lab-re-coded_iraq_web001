26dc55d0098651b3019260ff37acdffb51a65773
Neven Bedr <neven2225@gmail.com>
2020-07-09 17:45:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(record) {
+
+  const result = record.find(({result}) => result === 'W')
+
+  if(!!result){
+    return result.year + ''
+  }else{
+    return undefined
+  }
+
+}
